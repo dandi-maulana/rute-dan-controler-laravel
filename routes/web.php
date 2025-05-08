7aa148a8847214709ea007dfaab0d56708e29418
dandi-maulana <nugraha.rahmadan1304@gmail.com>
2025-05-08 11:37:00 +0700

    perubahan pertama untuk tugas tentang controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController; 
+use App\Http\Controllers\GreetingsController; 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () { 
+     return 'Selamat datang di aplikasi Laravel!'; 
+ }); 
+Route::get('/hello/{name}', function ($name) { 
+         return 'Hello, ' . $name . '!'; 
+         }); 
+
+Route::get('/', [HelloController::class, 'index']); 
+Route::get('/hello/{name}', [HelloController::class, 'sayHello']);
+
+Route::get('/', [GreetingsController::class, 'welcome']); 
+Route::get('/hello/{name}/{npm}', [GreetingsController::class, 'greet']);
+
